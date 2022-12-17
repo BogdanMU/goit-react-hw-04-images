efb2452a54eb1563c0d79b7a306e31f21868e0a7
BogdanMU <104568558+BogdanMU@users.noreply.github.com>
2022-12-17 19:06:27 +0200

    changed spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,5 +1,17 @@
-import { SpinningGlass } from './Spinner.styled';
+import { MutatingDots } from 'react-loader-spinner';
 
 export const Spinner = () => {
-  return <SpinningGlass />;
+  return (
+    <MutatingDots
+      height="100"
+      width="100"
+      color="#4fa94d"
+      secondaryColor="#4fa94d"
+      radius="12.5"
+      ariaLabel="mutating-dots-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
 };
