80ceff8f12843275fb29f1da06ea415e4106a0cf
BogdanMU <104568558+BogdanMU@users.noreply.github.com>
2022-12-18 14:51:01 +0200

    1

--- src/services/api.js
+++ src/services/api.js
@@ -19,7 +19,6 @@ export const getPictures = async (query, page) => {
       page: page,
     },
   };
-  // const searchQuery = `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
   const response = await axios.get('', config);
   return response.data;
 };
